a171fb795523fab63afe7f2b5db1ff1754208f3f
Indracit S <indrajithij802@gmail.com>
2023-08-20 18:17:12 +0530

    added charts

---
+++ client/src/components/Txn_BarChart.jsx
@@ -0,0 +1,36 @@
+
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+
+
+
+export default function Txn_Barchart({data}) {
+  return (
+    <BarChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{
+        top: 20,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="sucess" stackId="a" fill="#8884d8" />
+      <Bar dataKey="failure" stackId="a" fill="#82ca9d" />
+    </BarChart>
+  );
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,58 @@
+import '../styles/dashboard.scss'
+import Bclogin_chart from '../components/Bclogin_chart'
+import Txn_Barchart from '../components/Txn_BarChart'
 
+const data = [{ name: "Active Bcs", value: 100 },
+{ name: "Todays Active", value: 70},
+]
+
+const txn_data = [
+	{name: 'on_old',
+	sucess: 121938,
+	failure: 41657},
+	
+	{name: 'acq_old',
+	sucess: 14336,
+	failure: 6306},
+	
+	{name: 'tpd_old',
+	sucess: 19145,
+	failure: 1769},
+	
+	{name: 'enrl_old',
+	sucess: 3200,
+	failure: 1},
+	
+	{name: 'on',
+	sucess: 121938,
+	failure: 41657},
+	
+	{name: 'acq',
+	sucess: 14336,
+	failure: 6306},
+	
+	{name: 'tpd',
+	sucess: 19145,
+	failure: 1769},
+	
+	{name: 'enrl',
+	sucess: 3200,
+	failure: 1},
+	
+  ]
 
 const Dashboard = () => {
     return (
-        <div>
-            Dashboard
+        <div className='dashbaord'>
+            <span>
+            <Bclogin_chart data={data}/>
+            <p> Total bcs : 150</p>
+            </span>
+            
+            <span>
+                <Txn_Barchart data={txn_data}/>
+                <p> Txn data</p>
+            </span>
         </div>
     )
 }
